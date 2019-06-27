73e35ec327c0ddadcc367632cf38afaec858b1e9
stepyen <164987070@qq.com>
2019-06-27 22:54:36 +0800

    Initial commit

---
+++ app/src/main/java/com/stepyen/xpayDemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.stepyen.xpayDemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 支付宝、微信支付封装
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
